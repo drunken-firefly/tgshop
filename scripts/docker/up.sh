7bde56b13d1fb92f40866d0dd2dfaacde70fed77
drunken-firefly <126287086+drunken-firefly@users.noreply.github.com>
2023-03-07 08:01:11 +0300

    Небольшие изменения

--- scripts/docker/up.sh
+++ scripts/docker/up.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 # Создаем и запускаем контейнеры
 
+# Удаляем контейнеры
+source ./scripts/docker/unbuild.sh
+
+# Создаем контейнеры
+source ./scripts/docker/build.sh
+
+# Запускаем контейнеры
 sudo docker compose -f ./docker/docker-compose.yml --env-file ./docker/envs/general up -d
